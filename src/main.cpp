82d5c44102e934a6557d3f1de70f06c234a683aa
makrohn <makrohn@gmail.com>
2012-07-24 13:45:27 -0400

    Polymorphable displays in title bar instead of Flare

--- src/main.cpp
+++ src/main.cpp
@@ -121,7 +121,7 @@ static void init() {
 		Mix_Volume(-1, SOUND_VOLUME);
 
 	// Window title
-	const char* title = msg->get("Flare").c_str();
+	const char* title = msg->get("Polymorphable").c_str();
 	SDL_WM_SetCaption(title, title);
 
 
